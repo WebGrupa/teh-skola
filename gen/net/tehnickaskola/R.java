ef54bbbc980d128e98395a867abd3f799934722f
soundtech <soundtechofficial@gmail.com>
2013-09-27 18:53:18 +0200

    splash i tak

--- gen/net/tehnickaskola/R.java
+++ gen/net/tehnickaskola/R.java
@@ -25,22 +25,23 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int kalendar=0x7f020001;
-        public static final int obavijesti=0x7f020002;
-        public static final int raspored=0x7f020003;
-        public static final int testovi=0x7f020004;
+        public static final int background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int kalendar=0x7f020002;
+        public static final int logosplash=0x7f020003;
+        public static final int menu=0x7f020004;
+        public static final int obavijesti=0x7f020005;
+        public static final int raspored=0x7f020006;
+        public static final int testovi=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
-        public static final int button1=0x7f090005;
+        public static final int action_settings=0x7f090006;
         public static final int gridview=0x7f090001;
-        public static final int listView1=0x7f090007;
+        public static final int imgLogo=0x7f090004;
+        public static final int listView1=0x7f090005;
         public static final int listView2=0x7f090002;
         public static final int listView3=0x7f090003;
         public static final int listView4=0x7f090000;
-        public static final int spinner1=0x7f090004;
-        public static final int spinner2=0x7f090006;
     }
     public static final class layout {
         public static final int activity_biljeske=0x7f030000;
--- src/net/tehnickaskola/SplashActivity.java
+++ src/net/tehnickaskola/SplashActivity.java
@@ -3,23 +3,47 @@ package net.tehnickaskola;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+/**
+import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+*/
 
 
 public class SplashActivity extends Activity {
 
+	 private static int SPLASH_TIME_OUT = 3000;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);    // Removes title bar
+	      this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); 
 		setContentView(R.layout.activity_splash);
 		
+		new Handler().postDelayed(new Runnable() {
+			 
+            
+ 
+            @Override
+            public void run() {
+                
+                Intent i = new Intent(SplashActivity.this, MenuActivity.class);
+                startActivity(i);
+ 
+                
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
 		
-		
+	/**	
 	Button ok = (Button) findViewById(R.id.button1);
 	  
 	  ok.setOnClickListener(new OnClickListener() {
@@ -30,7 +54,7 @@ public class SplashActivity extends Activity {
 	    startActivity(intent);
 	   }
 	  });
-	  
+	  */
 	  
 }
 	
@@ -44,3 +68,4 @@ public class SplashActivity extends Activity {
 			
 	}	
 }
+		
